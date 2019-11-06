1ff607a36951abc0ba9a794c821d1366a2334e97
Federico Garcia <fedegarcia222@gmail.com>
2019-11-05 23:56:58 -0300

    Ejercicio terminado

--- admin.js
+++ admin.js
@@ -20,99 +20,99 @@ let pedidos = contFile.length > 0 ? JSON.parse(contFile) : null;
 //Lo hice de esta forma para probar cambiar con lo que se hizo en clase
 //Aunque quiza el .filter sea mas eficiente
 
-for(pedido of pedidos)
-{ 
-    switch(pedido.gustoPizza)
-    {
-        case 'Muzza':
-            pizzaMuzza++;
-            break;
-        case 'Napo':
-            pizzaNapo++;
-            break;
-        case 'Jamon y Morrones':
-            pizzaJYM++;
-            break;
-    }
-    switch(pedido.tipoPizza)
-    {
-        case 'Grande':
-            pizzaGrande++;
-            break;
-        case 'Mediana':
-            pizzaMediana++;
-            break;
-        case 'Personal':
-            pizzaPersonal++;
-            break;    
-    }
-    if(pedido.paraLlevar)
-    {
-        paraLlevar ++;
-    }
-    if(pedido.bebida)
-    {
-        bebidas++;
-    }
-    if(pedido.clienteHabitual)
-    {
-        clienteHabitual++;
-    }
+if(pedidos == null)
+{
+    console.log('Aun no se han registrado pedidos!');
 }
-let fecha = new Date().toLocaleDateString();
-let hora = new Date().toLocaleTimeString();
+else
+{ 
+    for(pedido of pedidos)
+    { 
+        switch(pedido.gustoPizza)
+        {
+            case 'Muzza':
+                pizzaMuzza++;
+                break;
+            case 'Napo':
+                pizzaNapo++;
+                break;
+            case 'Jamon y Morrones':
+                pizzaJYM++;
+                break;
+        }
+        switch(pedido.tipoPizza)
+        {
+            case 'Grande':
+                pizzaGrande++;
+                break;
+            case 'Mediana':
+                pizzaMediana++;
+                break;
+            case 'Personal':
+                pizzaPersonal++;
+                break;    
+        }
+        pedido.paraLlevar ? paraLlevar ++ : null;
+        
+        pedido.bebida ? bebidas++ : null;
+        
+        pedido.clienteHabitual ? clienteHabitual++ : null;
+        
+    }
+    let fecha = new Date().toLocaleDateString();
+    let hora = new Date().toLocaleTimeString();
 
-console.log('¡Reporte generado con éxito!');
-console.log('\n|===*** Reporte de ventas ***====|');
-console.log('Fecha de generacion: ' + fecha);
-console.log('Hora: ' + hora);
-console.log('\n|===*** Cantidad de pedidos realizados ***====|');
-console.log('Total: '+pedidos.length);
-console.log('\n|===*** Cantidad de pedidos para delivery ***====|');
-console.log('Total: ' + paraLlevar);
-console.log('\n|===*** Cantidad de pizzas vendidas por gusto ***====|');
-console.log('Total Muzzarella: ' + pizzaMuzza);
-console.log('Total Jamon y Morron: ' + pizzaJYM);
-console.log('Total Napolitana: ' + pizzaNapo);
-console.log('\n|===*** Cantidad de pizzas vendidas por tamaño ***====|');
-console.log('Total Personal: ' + pizzaPersonal);
-console.log('Total Mediana: ' + pizzaMediana);
-console.log('Total Grande: ' + pizzaGrande);
-console.log('\n|===*** Cantidad de pedidos con bebida ***====|');
-console.log('Total: ' + bebidas);
-console.log('\n|===*** Cantidad de clientes habituales ***====|');
-console.log('Total: ' + clienteHabitual);
-console.log('\n|===*** Cantidad de empanadas regaladas ***====|');
-console.log('Total: ' + (clienteHabitual*3));
+    console.log('¡Reporte generado con éxito!');
+    console.log('\n|===*** Reporte de ventas ***====|');
+    console.log('Fecha de generacion: ' + fecha);
+    console.log('Hora: ' + hora);
+    console.log('\n|===*** Cantidad de pedidos realizados ***====|');
+    console.log('Total: '+pedidos.length);
+    console.log('\n|===*** Cantidad de pedidos para delivery ***====|');
+    console.log('Total: ' + paraLlevar);
+    console.log('\n|===*** Cantidad de pizzas vendidas por gusto ***====|');
+    console.log('Total Muzzarella: ' + pizzaMuzza);
+    console.log('Total Jamon y Morron: ' + pizzaJYM);
+    console.log('Total Napolitana: ' + pizzaNapo);
+    console.log('\n|===*** Cantidad de pizzas vendidas por tamaño ***====|');
+    console.log('Total Personal: ' + pizzaPersonal);
+    console.log('Total Mediana: ' + pizzaMediana);
+    console.log('Total Grande: ' + pizzaGrande);
+    console.log('\n|===*** Cantidad de pedidos con bebida ***====|');
+    console.log('Total: ' + bebidas);
+    console.log('\n|===*** Cantidad de clientes habituales ***====|');
+    console.log('Total: ' + clienteHabitual);
+    console.log('\n|===*** Cantidad de empanadas regaladas ***====|');
+    console.log('Total: ' + (clienteHabitual*3));
 
-contenidoReporte = 
-`
-¡Reporte generado con éxito!
-\n|===*** Reporte de ventas ***====|
-Fecha de generacion: ${fecha}
-Hora: ${hora}
-\n|===*** Cantidad de pedidos realizados ***====|
-Total: ${pedidos.length}
-\n|===*** Cantidad de pedidos para delivery ***====|
-Total: ${paraLlevar}
-Total Muzzarella: ${pizzaMuzza}
-\n|===*** Cantidad de pizzas vendidas por gusto ***====|
-Total Jamon y Morron: ${pizzaJYM}
-Total Napolitana: ${pizzaNapo}
-\n|===*** Cantidad de pizzas vendidas por tamaño ***====|
-Total Personal: ${pizzaPersonal}
-Total Mediana: ${pizzaMediana}
-Total Grande: ${pizzaGrande}
-\n|===*** Cantidad de pedidos con bebida ***====|
-Total: ${bebidas}
-\n|===*** Cantidad de clientes habituales ***====|
-Total: ${clienteHabitual}
-\n|===*** Cantidad de empanadas regaladas ***====|
-Total: ${(clienteHabitual*3)}
-`;
+    contenidoReporte = 
+    `
+    ¡Reporte generado con éxito!
+    \n|===*** Reporte de ventas ***====|
+    Fecha de generacion: ${fecha}
+    Hora: ${hora}
+    \n|===*** Cantidad de pedidos realizados ***====|
+    Total: ${pedidos.length}
+    \n|===*** Cantidad de pedidos para delivery ***====|
+    Total: ${paraLlevar}
+    Total Muzzarella: ${pizzaMuzza}
+    \n|===*** Cantidad de pizzas vendidas por gusto ***====|
+    Total Jamon y Morron: ${pizzaJYM}
+    Total Napolitana: ${pizzaNapo}
+    \n|===*** Cantidad de pizzas vendidas por tamaño ***====|
+    Total Personal: ${pizzaPersonal}
+    Total Mediana: ${pizzaMediana}
+    Total Grande: ${pizzaGrande}
+    \n|===*** Cantidad de pedidos con bebida ***====|
+    Total: ${bebidas}
+    \n|===*** Cantidad de clientes habituales ***====|
+    Total: ${clienteHabitual}
+    \n|===*** Cantidad de empanadas regaladas ***====|
+    Total: ${(clienteHabitual*3)}
+    `;
 
-hora = hora.replace(/:/g,"");
-let pathReporte = __dirname + '\\'+'reporte-'+fecha+'-'+hora+'.txt';
+    hora = hora.replace(/:/g,"");
+    let pathReporte = __dirname + '\\'+'reporte-'+fecha+'-'+hora+'.txt';
 
-fs.writeFileSync(pathReporte,contenidoReporte);
-
+    fs.writeFileSync(pathReporte,contenidoReporte);
+}
